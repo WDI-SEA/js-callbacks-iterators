eb734db5bf5258edb60cec78e1231193a45fa24e
Taylor Darneille <33237160+TaylorDarneille@users.noreply.github.com>
2018-12-03 14:13:30 -0800

    Create filter2.js

---
+++ filter2.js
@@ -0,0 +1,5 @@
+var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite string", 59.09]
+
+// Use filter to filter all strings out of the above array.
+
+// Your code goes here

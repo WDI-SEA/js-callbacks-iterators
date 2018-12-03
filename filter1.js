ad59b5fd90f09418fe18ea65fa9f948c4a7ef172
Taylor Darneille <33237160+TaylorDarneille@users.noreply.github.com>
2018-12-03 14:13:11 -0800

    Update and rename filter.js to filter1.js

--- filter1.js
+++ filter1.js
@@ -24,9 +24,3 @@ var isCool = function(person) {
 }
 
 // Your code goes here
-
-var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite string", 59.09]
-
-// (2) Use filter to filter all strings out of the above array.
-
-// Your code goes here

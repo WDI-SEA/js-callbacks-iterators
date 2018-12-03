b846caba905f0ef6645b11b441aec3c21e09176a
Taylor Darneille <33237160+TaylorDarneille@users.noreply.github.com>
2018-12-03 13:58:11 -0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -17,15 +17,16 @@ var people = [
 	}
 ];
 
-// Your code here!
-
-// Write code that uses the filter function, and the below isCool function to return a list of people who are cool.
+// (1) Write code that uses the filter function, and the below isCool function to return a list of people who are cool.
 
 var isCool = function(person) {
 	return person.coolnessScore > 20;
 }
 
-// Example results:
-//
-// Bob is cool
-// Isolde is cool
+// Your code goes here
+
+var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite string", 59.09]
+
+// (2) Use filter to filter all strings out of the above array.
+
+// Your code goes here

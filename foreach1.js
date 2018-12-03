85657336dd235225e02831e4c20bfb165dfa253e
Taylor Darneille <33237160+TaylorDarneille@users.noreply.github.com>
2018-12-03 14:09:30 -0800

    Update foreach1.js

--- foreach1.js
+++ foreach1.js
@@ -1,13 +1,11 @@
-/*
-	For Each:
-	Use the .forEach iterator to loop over the following array of foods and say you like them.
- */
- 
-var foods = ["pizza", "tacos", "ice cream"];
 
-// your code here
+// (1) Use the .forEach iterator to loop over the following array of foods and say you like them.
 
 // The output should be
 // > "I like pizza"
 // > "I like tacos"
 // > "I like ice cream"
+ 
+var foods = ["pizza", "tacos", "ice cream", "sushi"];
+
+// your code here

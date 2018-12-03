c4f1e56c296db7498c419e6aa6fbc340c9e6151a
Taylor Darneille <33237160+TaylorDarneille@users.noreply.github.com>
2018-12-03 13:50:06 -0800

    Update map.js

--- map.js
+++ map.js
@@ -1,16 +1,51 @@
 var mneumonics = ['My Very Excellent Mother Just Sent Me Nine Pizzas', 'Please Excuse My Dear Aunt Sally', 'Every Good Boy Does Fine'];
-// Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
-
-// ex:
+// (1a) Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
 // For example, the 0th element of the new array should be:
 // ['My', 'Very', 'Excellent', 'Mother', 'Just', 'Sent', 'Me', 'Nine', 'Pizzas']
 
 // Your code here!
 
-// Now use map again to return a third array of objects were the key is the first letter of the word and the value is the whole word.
+// (1b) Now use map again to return a third array of objects were the key is the first letter of the word and the value is the whole word.
 // If there are two words that start with the same letter, add a number after it in the key to differentiate
 // For example, the 0th element should be:
 // { m: 'my', v: 'very', e: 'excellent', m2: 'mother', j: 'just', s: 'sent', m3: 'me', n: 'nine', p: 'pizzas' }
 
 // Your code here!
+
+//________________________________________________________________
+
+
+var phoneBook = [
+  {Abe: "111-111-1111"},
+   {Bob: "222-222-2222"},
+   {Cam: "333-333-3333"},
+   {Dan: "444-444-4444"},
+   {Ern: "555-555-5555"},
+   {Fry: "111-111-1111"},
+   {Gil: "222-222-2222"},
+   {Hal: "333-333-3333"},
+   {Ike: "444-444-4444"},
+   {Jim: "555-555-5555"},
+   {Kip: "111-111-1111"},
+   {Liv: "222-222-2222"},
+   {Mia: "333-333-3333"},
+   {Nik: "444-444-4444"},
+   {Oli: "555-555-5555"},
+   {Pam: "111-111-1111"},
+   {Qiq: "222-222-2222"},
+   {Rob: "333-333-3333"},
+   {Stu: "444-444-4444"},
+   {Tad: "555-555-5555"},
+   {Uwe: "111-111-1111"},
+   {Val: "222-222-2222"},
+   {Wil: "333-333-3333"},
+   {Xiu: "444-444-4444"},
+   {Yam: "555-555-5555"},
+   {Zed: "111-111-1111"}
+  ];
+
+// (3) Take the phonebook above and use map to return a new phonebook that includes a country-code that matches the rest of the phone number.
+// For example, the 0th element of the new array should be {Abe: "1-111-111-1111"}
+
+//Your code here
 

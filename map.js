d428e19c27fee74b485067edb1dddd06038c889b
Taylor Darneille <33237160+TaylorDarneille@users.noreply.github.com>
2018-12-03 13:23:44 -0800

    Update map.js

--- map.js
+++ map.js
@@ -1,15 +1,16 @@
 var mneumonics = ['My Very Excellent Mother Just Sent Me Nine Pizzas', 'Please Excuse My Dear Aunt Sally', 'Every Good Boy Does Fine'];
 // Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
 
-// Your code here!
-
-
 // ex:
 // For example, the 0th element of the new array should be:
 // ['My', 'Very', 'Excellent', 'Mother', 'Just', 'Sent', 'Me', 'Nine', 'Pizzas']
+
+// Your code here!
 
 // Now use map again to return a third array of objects were the key is the first letter of the word and the value is the whole word.
 // If there are two words that start with the same letter, add a number after it in the key to differentiate
 // For example, the 0th element should be:
 // { m: 'my', v: 'very', e: 'excellent', m2: 'mother', j: 'just', s: 'sent', m3: 'me', n: 'nine', p: 'pizzas' }
+
+// Your code here!
 

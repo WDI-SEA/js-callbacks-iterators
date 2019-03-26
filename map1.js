882c20dcad7295c8f04c8be7213ee1d1b09f5c8c
Brandi Williams <brandimichelle.0911@gmail.com>
2019-03-25 22:05:31 -0700

    Removing weird not map question

--- map1.js
+++ map1.js
@@ -1,14 +1,8 @@
 var mneumonics = ['My Very Excellent Mother Just Sent Me Nine Pizzas', 'Please Excuse My Dear Aunt Sally', 'Every Good Boy Does Fine'];
-// (a) Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
+
+// Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
 // For example, the 0th element of the new array should be:
 // ['My', 'Very', 'Excellent', 'Mother', 'Just', 'Sent', 'Me', 'Nine', 'Pizzas']
-
-// Your code here!
-
-// (b) Now use map again to return a third array of objects were the key is the first letter of the word and the value is the whole word.
-// If there are two words that start with the same letter, add a number after it in the key to differentiate
-// For example, the 0th element should be:
-// { m: 'my', v: 'very', e: 'excellent', m2: 'mother', j: 'just', s: 'sent', m3: 'me', n: 'nine', p: 'pizzas' }
 
 // Your code here!
 

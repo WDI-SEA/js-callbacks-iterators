a2017b3d03f3b0223cfa0f6bbfa7e006665016cc
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-16 22:25:34 -0700

    [deliverable] Add map1

--- map1.js
+++ map1.js
@@ -1,8 +1,13 @@
 var mneumonics = ['My Very Excellent Mother Just Sent Me Nine Pizzas', 'Please Excuse My Dear Aunt Sally', 'Every Good Boy Does Fine'];
 
-// Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
+// Write some code using map that takes the above array of mneumonics,
+//and returns a two dimensional array.
 // For example, the 0th element of the new array should be:
 // ['My', 'Very', 'Excellent', 'Mother', 'Just', 'Sent', 'Me', 'Nine', 'Pizzas']
 
 // Your code here!
 
+var splitNemos = mneumonics.map(function(nemo) {
+    return nemo.split(" ");
+});
+console.log(splitNemos);

87f3dac59ababc0d146cde8852aaa17822221166
Elaine <elaine@Felipes-iMac.local>
2020-07-17 01:45:22 -0500

    [filter2] adds my homework code.

--- filter2.js
+++ filter2.js
@@ -4,3 +4,13 @@ var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite
 // Don't worry about strings in the nested array (misc[5] is considered an array, not a string)
 
 // Your code goes here
+
+function filterMisc(value) {
+    for (let i = 0; value > i; i++) {
+        if (value = String) {
+            return value;
+        }
+    }
+}
+var result = misc.filter(filterMisc)
+console.log(result);

0386774b3d784288cf82495655af3f4f7547e2cb
Hana <>
2020-07-17 09:20:34 -0700

    altered function for foeeach2

--- foreach2.js
+++ foreach2.js
@@ -12,13 +12,5 @@
 
 // your code here
 foods.forEach(e => {
- if (e.level === "very") {
-  console.log(`${e.name} is very delicious`);
- } else if (e.level === "mostly") {
-  console.log(`${e.name} is most delicious`);
- } else if (e.level === "not very") {
-  console.log(`${e.name} is not very delicious`);
- } else {
-  console.log(`i don't have an opinion on that yet`);
- }
-})
+  console.log(`${e.name} is ${e.level} delicious`);
+});

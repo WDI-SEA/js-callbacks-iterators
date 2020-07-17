046b6256937bff7fb994754710b15be98119781f
Elaine <elaine@Felipes-iMac.local>
2020-07-16 22:44:21 -0500

    [map1] creates my code homework.

--- map1.js
+++ map1.js
@@ -1,5 +1,13 @@
 var mneumonics = ['My Very Excellent Mother Just Sent Me Nine Pizzas', 'Please Excuse My Dear Aunt Sally', 'Every Good Boy Does Fine'];
 
+function splitMneumonics(array){
+return {
+    newSentence: array.split(" "),
+}
+
+}
+var newCode = mneumonics.map(splitMneumonics);
+console.log(newCode);
 // Write some code using map that takes the above array of mneumonics, and returns a two dimensional array.
 // For example, the 0th element of the new array should be:
 // ['My', 'Very', 'Excellent', 'Mother', 'Just', 'Sent', 'Me', 'Nine', 'Pizzas']

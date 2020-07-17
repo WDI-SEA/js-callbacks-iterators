bdff6b90f50a2d29cc69383f73cc57aca44d2f62
imjchiang <jchiang296@gmail.com>
2020-07-17 10:25:36 -0700

    [JS] revise filter 2 from noString to allString

--- filter2.js
+++ filter2.js
@@ -4,6 +4,7 @@ var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite
 // Don't worry about strings in the nested array (misc[5] is considered an array, not a string)
 
 // Your code goes here
+/*
 const noString = misc.filter(element => {
     if (typeof element !== "string")
     {
@@ -12,3 +13,30 @@ const noString = misc.filter(element => {
 } );
 
 console.log(noString);
+*/
+let result = [];
+
+const allString = misc.filter(element => 
+{
+    if (typeof element === "string")
+    {
+        result.push(element);
+    }
+    else if (Array.isArray(element)) 
+    {
+        getArrayString(element);
+    }
+} );
+
+function getArrayString(array)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        if (typeof array[i] === "string")
+        {
+            result.push(array[i])
+        } 
+    }
+}
+
+console.log(result);

261c88a6230469e6c3d67f128d0472ef97c4b066
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-07-17 11:21:47 -0600

    [iterators] add getAllStrings function to filter2

--- filter2.js
+++ filter2.js
@@ -8,6 +8,27 @@ var misc = [3, "true", {a: 1, b: 2}, 7, [1, 2, 3], ['a', 'b', 'c'], "my favorite
 const answer = misc.filter(element => {
     if (typeof element == 'string') {
         return element;
-    }
-})
+    }});
 console.log(answer);
+
+// advanced...
+
+function getAllString(array) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let element = array[i];
+
+        if(typeof element === 'string') {
+            result.push(element);
+        } else if (Array.isArray(element)) {
+            element.forEach(ele => {
+                if (typeof ele == 'string') {
+                    result.push(ele);
+                }
+            })
+        }
+    }
+    return result;
+}
+console.log(getAllString(misc));

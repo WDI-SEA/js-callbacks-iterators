c1f6e880c9ece536974a41bf210ee67374b4063d
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-09-25 22:21:51 +0300

    map1 Done

--- map1.js
+++ map1.js
@@ -1,42 +1,48 @@
 let forecast = [
     {
-        day: 'Monday',
-        high: 55,
-        low: 53,
+      day: 'Monday',
+      high: 55,
+      low: 53,
     },
     {
-        day: 'Tuesday',
-        high: 65,
-        low: 60,
+      day: 'Tuesday',
+      high: 65,
+      low: 60,
     },
     {
-        day: 'Wednesday',
-        high: 63,
-        low: 58,
+      day: 'Wednesday',
+      high: 63,
+      low: 58,
     },
     {
-        day: 'Thursday',
-        high: 61,
-        low: 57,
+      day: 'Thursday',
+      high: 61,
+      low: 57,
     },
     {
-        day: 'Friday',
-        high: 60,
-        low: 53,
+      day: 'Friday',
+      high: 60,
+      low: 53,
     },
     {
-        day: 'Saturday',
-        high: 61,
-        low: 58,
+      day: 'Saturday',
+      high: 61,
+      low: 58,
     },
     {
-        day: 'Sunday',
-        high: 57,
-        low: 54,
+      day: 'Sunday',
+      high: 57,
+      low: 54,
     },
-]
-
-// Build an array of strings using the forecast objects above with the following pattern:
-// "Monday will have a high of 55F and a low of 53F."
-
-//Your code here
+  ]
+  
+  // Build an array of strings using the forecast objects above with the following pattern:
+  // "Monday will have a high of 55F and a low of 53F."
+  
+  //Your code here
+  
+  const f=forecast.map((forecast)=> forecast.day+" will have a high of "+ forecast.high +" and a low of "+forecast.low)
+  
+  console.log(f)
+  
+  

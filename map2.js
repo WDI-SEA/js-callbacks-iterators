633b0c172de7d6e1298387e3c6fcb44d00489231
Anwaar <anwaaralsyouf@gmail.com>
2022-09-25 17:45:57 +0300

    soloution 6 added

--- map2.js
+++ map2.js
@@ -6,3 +6,25 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // e.g. "36 is larger than 30"
 
 //Your code here
+
+//1- sequaring each number:
+let sequare = numbers.map((num) => {
+
+    return Math.pow(num, 2)
+  })
+  
+  console.log(sequare)
+  
+  //2- -filter the resulting array:
+  let largerThan = sequare.filter((num)=>{
+      if (num > 30) {
+        return true
+    }
+  })
+  
+  console.log(largerThan)
+  
+  //3- print 
+  let printNumbers = largerThan.forEach((num)=>{
+    console.log(num + " is larger than 30")
+  })

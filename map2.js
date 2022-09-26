9ec21ad0226b52e52ecd402c87148f8227554efb
Sabika Ali <Sabikaalghadeeri@gmail.com>
2022-09-26 13:17:42 +0300

    UPDATED

--- map2.js
+++ map2.js
@@ -6,3 +6,18 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // e.g. "36 is larger than 30"
 
 //Your code here
+
+const square = numbers.map(num => num * num)
+
+console.log(square)
+
+
+const filter = square.filter((squar) => {
+  return squar > 30
+})
+
+console.log(filter)
+
+for (let i = 0; i < filter.length; i++) {
+  console.log(filter[i] + " is larger than 30")
+}
